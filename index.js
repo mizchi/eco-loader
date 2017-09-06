947be9aa0537c5f0ddbed36bba21e5ef075c5495
mizchi <miz404@gmail.com>
2017-09-06 16:05:08 +0900

    Named

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ var eco = require('eco');
 module.exports = function (source) {
   this.cacheable && this.cacheable();
   var template = eco.precompile(source);
+  // return 'module.exports = ' + template;
   var name = this.resourcePath;
-  return 'global.JST=global.JST||{}; JST["' + name + '"] = module.exports = ' + template;
+  var paths = name.split('/').map(function(n) { return n.replace('.jst.eco', '') }).reverse()
+  var ret = []
+  for (var i = 0; i < paths.length; i++) {
+    var path = paths[i];
+    if (path === 'templates') {
+      break
+    }
+    ret.push(path)
+  }
+  var tname = ret.reverse().join('/')
+  return 'global.JST=global.JST||{}; JST["' + tname + '"]='  + template;
 };
+
